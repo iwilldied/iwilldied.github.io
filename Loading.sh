e0f20524a2d9bf0291033633c2fc180e323fc4ca
iwilldied <nakrothkhieuchien@gmail.com>
2021-12-28 23:15:44 +0700

    up

--- Loading.sh
+++ Loading.sh
@@ -5,7 +5,7 @@ if [[ -e compatity.txt ]]; then
     compatity=$(cat compatity.txt)
 fi
 
-for i in DEB/*.deb
+for i in Deb/*.deb
 do
    debInfo=`dpkg -f $i`
    pkg=`echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`
@@ -50,7 +50,7 @@ echo "{}]" >> all.pkgs
 }
 echo "------------------"
 echo "Đang tạo Packages...."
-apt-ftparchive packages ./DEB > ./Packages;
+apt-ftparchive packages ./Deb > ./Packages;
 #sed -i -e '/^SHA/d' ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
 echo "------------------"
