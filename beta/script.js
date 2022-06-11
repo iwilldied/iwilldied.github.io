a751a0a0fbae2cf1e5ec5234c752ec717f65ee9b
iwilldied <nakrothkhieuchien0@gmail.com>
2022-06-11 22:22:12 +0700

    up

--- beta/script.js
+++ beta/script.js
@@ -1,1 +1,7 @@
-"😺"
+(function() {
+
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyIiXX0=
+//# sourceURL=coffeescript
